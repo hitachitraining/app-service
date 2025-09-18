b075a1f486271278e724e5319ef71968a89608e3
weirdly0 <106332839+weirdly0@users.noreply.github.com>
2025-09-18 16:25:35 +0530

    added the project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello to hitachi energy 🚀');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
